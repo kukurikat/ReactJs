0ff1c8748c23510bdf9954ce2c360e73ee2d51d0
kukurikat <maks2701207@gmail.com>
2025-04-04 15:50:30 +0300

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import List from "./List";
-
 function App() {
-  const fruits = [
-    { name: "banana", callories: 100 },
-    { name: "apple", callories: 200 },
-    { name: "orange", callories: 300 },
-  ];
-  const vegetables = [
-    { name: "carrot", callories: 50 },
-    { name: "broccoli", callories: 70 },
-    { name: "spinach", callories: 30 },
-  ];
-  return (
-    <>
-      <List listtoget={fruits} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
